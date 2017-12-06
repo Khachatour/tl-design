a9f9cc3985ee9db394d748e648d70d7d837f875f
Khachatour <loledgamer96@hotmail.com>
2017-12-06 21:34:40 +0400

    core: ui for section 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import Paragraph from 'grommet/components/Paragraph'
 import Box from 'grommet/components/Box'
 import Section from 'grommet/components/Section'
 import Article from 'grommet/components/Article'
+import Heading from 'grommet/components/Heading'
 import Headline from 'grommet/components/Headline'
+import Card from 'grommet/components/Card'
+import Tiles from 'grommet/components/Tiles'
+import Tile from 'grommet/components/Tile'
 import './App.css'
 
 class App extends Component {
@@ -20,8 +24,7 @@ class App extends Component {
 
   render() {
     return (
-      <Article scrollStep
-        controls>
+      <Article scrollStep controls full>
         <Section pad='none' >
           <div className='web-wrapper'>
             <div className='shadow-overlay'>
@@ -36,22 +39,42 @@ class App extends Component {
             </div>
           </div>
         </Section>
-        <Section pad='large'
-          justify='center'
-          align='center'
-          full='vertical'
-          colorIndex='grey-4'>
-          <Headline margin='none'>
-            Section 2
-         </Headline>
+        <Section align='center' full='vertical'>
+          <Box pad='large'>
+            <Heading align='center' uppercase strong tag='h2'>What we offer</Heading>
+          </Box>
+          <Tiles fill>
+            <Tile>
+              <Card thumbnail='https://www.raic.org/sites/default/files/news_image/temple_web.jpg'
+                heading='Sample Heading'
+                label='Sample Label'
+                description='Sample description providing more details.' />
+            </Tile>
+            <Tile>
+              <Card thumbnail='https://www.raic.org/sites/default/files/news_image/temple_web.jpg'
+                heading='Sample Heading'
+                label='Sample Label'
+                description='Sample description providing more details.' />
+            </Tile>
+            <Tile>
+              <Card thumbnail='https://www.raic.org/sites/default/files/news_image/temple_web.jpg'
+                heading='Sample Heading'
+                label='Sample Label'
+                description='Sample description providing more details.' />
+            </Tile>
+            <Tile>
+              <Card thumbnail='https://www.raic.org/sites/default/files/news_image/temple_web.jpg'
+                heading='Sample Heading'
+                label='Sample Label'
+                description='Sample description providing more details.' />
+            </Tile>
+          </Tiles>
         </Section>
         <Section pad='large'
           justify='center'
           align='center'
           full='vertical'>
-          <Headline margin='none'>
-      Section 3
-    </Headline>
+          <Headline margin='none'>Section 3</Headline>
         </Section>
       </Article>
     )
