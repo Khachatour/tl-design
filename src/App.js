87eb8eb2dc2fcd9d87f06aa1bd36df985481b5dd
Khachatour <loledgamer96@hotmail.com>
2017-12-13 14:50:13 +0400

    core: main layout

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Heading from 'grommet/components/Heading'
 import Headline from 'grommet/components/Headline'
 import Card from 'grommet/components/Card'
 import Label from 'grommet/components/Label'
+import Typist from 'react-typist'
+
 import './App.css'
 
 class App extends Component {
@@ -31,21 +33,25 @@ class App extends Component {
                 <AnimatedHeading />
                 <Box align='center'>
                   <Paragraph align='center'>
-                    Architecture and design studio that meets your needs
+                    <Typist avgTypingDelay={80}>
+                      {`Architecture and design studio that fucks yo${(' '.repeat(3))}`}
+                      <Typist.Backspace count={11} />
+                    meets your needs
+                    </Typist>
                   </Paragraph>
                 </Box>
               </div>
             </div>
           </div>
         </Section>
-        <Section>
+        <Section style={{ minHeight: '100vh' }}>
           <Box pad='large'>
             <Heading align='center' uppercase strong tag='h2'>What we offer</Heading>
           </Box>
           <Box direction='row'
             justify='center'
             align='center'>
-            <Box basis='1'
+            <Box
               align='center'
               pad='medium'>
               <Box className='my-container'>
@@ -55,7 +61,7 @@ class App extends Component {
                   label={<Label size='small' uppercase style={{ color: 'white' }}>design</Label>} />
               </Box>
             </Box>
-            <Box basis='1'
+            <Box
               align='center'
               pad='medium'>
               <Box className='my-container'>
@@ -69,7 +75,7 @@ class App extends Component {
           <Box direction='row'
             justify='center'
             align='center'>
-            <Box basis='1'
+            <Box
               align='center'
               pad='medium'>
               <Box className='my-container'>
@@ -79,7 +85,7 @@ class App extends Component {
                   label={<Label size='small' uppercase style={{ color: 'white' }}>architecture</Label>} />
               </Box>
             </Box>
-            <Box basis='1'
+            <Box
               align='center'
               pad='medium'>
               <Box className='my-container'>
@@ -91,11 +97,23 @@ class App extends Component {
             </Box>
           </Box>
         </Section>
-        <Section pad='large'
-          justify='center'
-          align='center'
-          full='vertical'>
-          <Headline margin='none'>Section 3</Headline>
+        <Section style={{ minHeight: '100vh' }} pad='none'>
+          <Box justify='start'
+            full='vertical'
+            colorIndex='neutral-4'>
+            <Box direction='row' pad='large' basis='xxlarge'>
+              <Paragraph size='large' className='last-page-paragraph'>
+                Raised on hip-hop and foster care, defiant city kid Ricky
+              gets a fresh start in the New Zealand countryside. He quickly finds himself
+              at home with his new foster family: the loving Aunt Bella, the cantankerous
+              Uncle Hec, and dog Tupac. When a tragedy strikes that threatens to ship
+              Ricky to another home, both he and Hec go on the run in the bush. As a
+              national manhunt ensues, the newly branded outlaws must face their
+              options: go out in a blaze of glory or overcome their differences and
+              survive as a family.
+                </Paragraph>
+            </Box>
+          </Box>
         </Section>
       </Article>
     )
@@ -103,3 +121,12 @@ class App extends Component {
 }
 
 export default App
+
+// <Heading uppercase={false} align='center'>
+//             enough talking,
+//             <Heading margin='large' tag='h3' align='center' uppercase strong className='test-text'>see</Heading>
+//             what we have done, or
+//             <Heading margin='large' tag='h3' align='center' uppercase strong className='test-text'>read</Heading>
+//             our blog, and if you want check out our
+//             <Heading margin='large' tag='h3' align='center' uppercase strong className='test-text'>news</Heading>
+//             </Heading>
